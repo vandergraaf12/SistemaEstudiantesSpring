b9ed3802df957a709987374b334c64cf77b2ef7f
HIMT <himt823@gmail.com>
2023-10-21 14:42:36 -0300

    Se añaden metodos CRUD del servicio Estudiante

--- src/main/java/himt/estudiantes/servicio/EstudianteServicio.java
+++ src/main/java/himt/estudiantes/servicio/EstudianteServicio.java
@@ -1,4 +1,35 @@
 package himt.estudiantes.servicio;
 
+import himt.estudiantes.modelo.Estudiante;
+import himt.estudiantes.repositorio.EstudianteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class EstudianteServicio implements IEstudianteServicio{
+
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+    }
 }
